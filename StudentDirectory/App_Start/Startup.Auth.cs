3905de4d111b2750381381abead10d074d90cc61
gc200362901 <200362901@student.georgianc.on.ca>
2018-03-26 19:58:50 -0400

    twitter auth

--- StudentDirectory/App_Start/Startup.Auth.cs
+++ StudentDirectory/App_Start/Startup.Auth.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Twitter;
 using Owin;
 using StudentDirectory.Models;
 
@@ -51,9 +52,26 @@ namespace StudentDirectory
                 clientId: ConfigurationManager.AppSettings["MicrosoftClientId"],
                 clientSecret: ConfigurationManager.AppSettings["MicrosoftClientSecret"]);
 
-            app.UseTwitterAuthentication(
-            consumerKey: "p1BHccsAemTwOVNP0kk6hkerI",
-            consumerSecret: "TmeET0oFLuPkLg17Wz00PR2c4aS2SuyNrKKmmb99H2JBieR7sQ");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = "p1BHccsAemTwOVNP0kk6hkerI",
+                ConsumerSecret = "TmeET0oFLuPkLg17Wz00PR2c4aS2SuyNrKKmmb99H2JBieR7sQ",
+                BackchannelCertificateValidator = new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
+    {
+        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+        "‎add53f6680fe66e383cbac3e60922e3b4c412bed", // Symantec Class 3 EV SSL CA - G3
+        "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+        "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
+        "B13EC36903F8BF4701D498261A0802EF63642BC3" // DigiCert High Assurance EV Root CA
+    })
+            });
+
+            //app.UseTwitterAuthentication(
+            //consumerKey: "",
+            //consumerSecret: "");
 
             //app.UseFacebookAuthentication(
             //   appId: "",
